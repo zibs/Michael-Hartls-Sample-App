6a490d5a1fe571a299edaad6da6cf4092caea771
zibs <elizibin@gmail.com>
2014-10-25 21:19:17 +0000

    Finish layout/routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
   
 end
 
